df8f94dab45c3d92eb737467b0e01ba196c6dd20
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-12-05 17:21:28 -0600

    Add RemoteRef interface for pcy and group refs
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ api/v1/interfaces.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	intoto "github.com/in-toto/attestation/go/v1"
+)
+
+var _ RemoteReference = &PolicyRef{}
+var _ RemoteReference = &PolicyGroupRef{}
+
+// RemoteReference is an interface to handle policy and group references
+type RemoteReference interface {
+	GetId() string
+	GetIdentity() *Identity
+	GetLocation() *intoto.ResourceDescriptor
+	GetSourceURL() string
+	GetVersion() int64
+	SetVersion(int64)
+}
