cd4e7708c11a121db238917facab0972f9848063
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-12-06 15:20:14 -0600

    Add compiler and parser tests for policies
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

--- compiler_test.go
+++ compiler_test.go
@@ -121,7 +121,7 @@ func TestCompileLocalGroups(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Simple policy set tp test locally defined policy groups", set.GetMeta().GetDescription())
-		require.Len(t, set.GetPolicies(), 0)
+		require.Empty(t, set.GetPolicies())
 		require.Len(t, set.GetGroups(), 1)
 		require.Len(t, set.GetGroups()[0].GetBlocks(), 4)
 		require.Equal(t, "single-passing", set.GetGroups()[0].GetBlocks()[0].GetId())
@@ -153,11 +153,68 @@ func TestCompileLocalGroups(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Policy set testing remote referenced group", set.GetMeta().GetDescription())
-		require.Len(t, set.GetPolicies(), 0)
+		require.Empty(t, set.GetPolicies())
 		require.Len(t, set.GetGroups(), 1)
 		require.Equal(t, "Group testing the assert modes", set.GetGroups()[0].GetMeta().GetDescription())
 		require.Len(t, set.GetGroups()[0].GetBlocks(), 4)
 		require.Equal(t, "single-passing", set.GetGroups()[0].GetBlocks()[0].GetId())
 		require.Len(t, set.GetGroups()[0].GetBlocks()[1].GetPolicies(), 2)
+	})
+}
+
+// Policy Compiling Tests
+func TestCompileLocalPolicies(t *testing.T) {
+	t.Parallel()
+	t.Run("test-localset-policy", func(t *testing.T) {
+		t.Parallel()
+		set, err := NewParser().ParsePolicySetFile("testdata/policies-local.json")
+		require.NoError(t, err)
+		require.NotNil(t, set)
+
+		set, err = NewCompiler().CompileSet(set)
+		require.NoError(t, err)
+
+		require.Equal(t, "Simple policy set to test locally defined policies", set.GetMeta().GetDescription())
+		require.Len(t, set.GetPolicies(), 2)
+		require.Empty(t, set.GetGroups())
+		require.Equal(t, "local-policy-1", set.GetPolicies()[0].GetId())
+		require.Equal(t, "AND", set.GetPolicies()[0].GetMeta().GetAssertMode())
+		require.Len(t, set.GetPolicies()[0].GetTenets(), 2)
+		require.Equal(t, "local-policy-2", set.GetPolicies()[1].GetId())
+		require.Equal(t, "OR", set.GetPolicies()[1].GetMeta().GetAssertMode())
+	})
+
+	t.Run("test-local-policy", func(t *testing.T) {
+		t.Parallel()
+		policy, err := NewParser().ParsePolicyFile("testdata/policy.single.json")
+		require.NoError(t, err)
+		require.NotNil(t, policy)
+
+		policy, err = NewCompiler().CompilePolicy(policy)
+		require.NoError(t, err)
+
+		require.Equal(t, "Policy testing the assert modes and tenets", policy.GetMeta().GetDescription())
+		require.Equal(t, "policy-assert-mode-test", policy.GetId())
+		require.Equal(t, "AND", policy.GetMeta().GetAssertMode())
+		require.Len(t, policy.GetTenets(), 2)
+		require.Equal(t, "tenet-1", policy.GetTenets()[0].GetId())
+		require.Equal(t, "tenet-2", policy.GetTenets()[1].GetId())
+	})
+
+	t.Run("test-remote-policy", func(t *testing.T) {
+		t.Parallel()
+		set, err := NewParser().ParsePolicySetFile("testdata/policy.remoteref.json")
+		require.NoError(t, err)
+		require.NotNil(t, set)
+
+		set, err = NewCompiler().CompileSet(set)
+		require.NoError(t, err)
+
+		require.Equal(t, "Policy set testing remote referenced policy", set.GetMeta().GetDescription())
+		require.Len(t, set.GetPolicies(), 1)
+		require.Empty(t, set.GetGroups())
+		require.Equal(t, "SBOM data found", set.GetPolicies()[0].GetMeta().GetDescription())
+		require.NotEmpty(t, set.GetPolicies()[0].GetId())
+		require.NotNil(t, set.GetPolicies()[0].GetTenets())
 	})
 }
--- parser_test.go
+++ parser_test.go
@@ -18,7 +18,7 @@ func TestParseLocalGroups(t *testing.T) {
 		require.NotNil(t, set)
 
 		require.Equal(t, "Simple policy set tp test locally defined policy groups", set.GetMeta().GetDescription())
-		require.Len(t, set.GetPolicies(), 0)
+		require.Empty(t, set.GetPolicies())
 		require.Len(t, set.GetGroups(), 1)
 		require.Len(t, set.GetGroups()[0].GetBlocks(), 4)
 		require.Equal(t, "single-passing", set.GetGroups()[0].GetBlocks()[0].GetId())
@@ -36,5 +36,37 @@ func TestParseLocalGroups(t *testing.T) {
 		require.Equal(t, "single-passing", grp.GetBlocks()[0].GetId())
 		require.Len(t, grp.GetBlocks()[1].GetPolicies(), 2)
 	})
+}
 
+func TestParseLocalPolicies(t *testing.T) {
+	t.Parallel()
+	t.Run("test-parse-localset-policy", func(t *testing.T) {
+		t.Parallel()
+		set, err := NewParser().ParsePolicySetFile("testdata/policies-local.json")
+		require.NoError(t, err)
+		require.NotNil(t, set)
+
+		require.Equal(t, "Simple policy set to test locally defined policies", set.GetMeta().GetDescription())
+		require.Len(t, set.GetPolicies(), 2)
+		require.Empty(t, set.GetGroups())
+		require.Equal(t, "local-policy-1", set.GetPolicies()[0].GetId())
+		require.Equal(t, "AND", set.GetPolicies()[0].GetMeta().GetAssertMode())
+		require.Len(t, set.GetPolicies()[0].GetTenets(), 2)
+		require.Equal(t, "local-policy-2", set.GetPolicies()[1].GetId())
+		require.Equal(t, "OR", set.GetPolicies()[1].GetMeta().GetAssertMode())
+	})
+
+	t.Run("test-parse-local-policy", func(t *testing.T) {
+		t.Parallel()
+		policy, err := NewParser().ParsePolicyFile("testdata/policy.single.json")
+		require.NoError(t, err)
+		require.NotNil(t, policy)
+
+		require.Equal(t, "Policy testing the assert modes and tenets", policy.GetMeta().GetDescription())
+		require.Equal(t, "policy-assert-mode-test", policy.GetId())
+		require.Equal(t, "AND", policy.GetMeta().GetAssertMode())
+		require.Len(t, policy.GetTenets(), 2)
+		require.Equal(t, "tenet-1", policy.GetTenets()[0].GetId())
+		require.Equal(t, "tenet-2", policy.GetTenets()[1].GetId())
+	})
 }
